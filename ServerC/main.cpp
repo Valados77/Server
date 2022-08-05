6f791e66e751e500508fd783f79f6d8378c49066
Vladislav <vprokopovich2002@gmail.com>
2022-08-05 18:15:47 +0300

    Replacing the messenger with a file transfer

--- ServerC/main.cpp
+++ ServerC/main.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
+#pragma warning(disable: 4996)
+#pragma comment(lib, "Ws2_32.lib")
 #include <WinSock2.h>
 #include <WS2tcpip.h>
-#include <stdio.h>
-#include <vector>
-#include <fstream>
 
-#pragma comment(lib, "Ws2_32.lib")
+using namespace std;
 
-//const char SERVER_IP[] = "";					// Enter IPv4 address of Server
-//const short SERVER_PORT_NUM = 0;				// Enter Listening port on Server side
-const short BUFF_SIZE = 1024;
-
-void RecvFile(SOCKET* sock) {
+void recv_file(SOCKET* sock) {
 	char file_size_str[16];
 	char file_name[32];
 
@@ -41,8 +39,9 @@ void RecvFile(SOCKET* sock) {
 }
 
 int main() {
-	WSADATA wsData;
-	if (WSAStartup(MAKEWORD(2, 2), &wsData) != 0) {
+	WSADATA wsData; // структура содержащая детали реализации Windows Sockets
+	if (WSAStartup(MAKEWORD(2, 2), &wsData) != 0) { //a-максимальная версия Windows Sockets; b-указатель на структуру WSADATA
+		//wsData получает информацию о версии WinSock 
 		std::cout << "Error WinSock version initializaion #" << WSAGetLastError();
 		return 1;
 	}
@@ -57,71 +56,28 @@ int main() {
 	}
 	else	std::cout << "Server socket initialization is OK" << '\n';
 
-	in_addr ip_to_num;
-	if (inet_pton(AF_INET, "127.0.0.1", &ip_to_num) <= 0) {
-		std::cout << "Error in IP translation to special numeric format" << '\n';
-		return 1;
-	}
+	sockaddr_in addr_info;
+	ZeroMemory(&addr_info, sizeof(addr_info));
 
-	sockaddr_in servInfo;
-	ZeroMemory(&servInfo, sizeof(servInfo));
-	servInfo.sin_family = AF_INET;
-	servInfo.sin_addr = ip_to_num;
-	servInfo.sin_port = htons(1111);
+	int size_addr = sizeof(addr_info);
+	InetPton(AF_INET, L"127.0.0.1", &addr_info.sin_addr);
+	addr_info.sin_port = htons(1111);
+	addr_info.sin_family = AF_INET;
 
-	if (connect(ClientSock, (sockaddr*)&servInfo, sizeof(servInfo)) != 0) {
+
+	if (connect(ClientSock, (sockaddr*)&addr_info, sizeof(addr_info)) != 0) {
 		std::cout << "Connection to Server is FAILED. Error # " << WSAGetLastError() << '\n';
 		closesocket(ClientSock);
 		WSACleanup();
 		return 1;
 	}
 	else {
-		std::cout << "Connection established SUCCESSFULLY. Ready to send a message to Server" << '\n';
-		RecvFile(&ClientSock);
+		std::cout << "Connection established SUCCESSFULLY. Ready to receive a picture from Server" << '\n';
 	}
-
-
-	//std::vector <char> servBuff(BUFF_SIZE), clientBuff(BUFF_SIZE);							// Buffers for sending and receiving data
-	//short packet_size = 0;												// The size of sending / receiving packet in bytes
-
-	//while (true) {
-
-	//	std::cout << "Your (Client) message to Server: ";
-	//	fgets(clientBuff.data(), clientBuff.size(), stdin);
-
-	//	// Check whether client like to stop chatting 
-	//	if (clientBuff[0] == 'x' && clientBuff[1] == 'x' && clientBuff[2] == 'x') {
-	//		shutdown(ClientSock, SD_BOTH);
-	//		closesocket(ClientSock);
-	//		WSACleanup();
-	//		return 0;
-	//	}
-
-	//	packet_size = send(ClientSock, clientBuff.data(), clientBuff.size(), 0);
-
-	//	if (packet_size == SOCKET_ERROR) {
-	//		std::cout << "Can't send message to Server. Error # " << WSAGetLastError() << '\n';
-	//		closesocket(ClientSock);
-	//		WSACleanup();
-	//		return 1;
-	//	}
-
-	//	packet_size = recv(ClientSock, servBuff.data(), servBuff.size(), 0);
-
-	//	if (packet_size == SOCKET_ERROR) {
-	//		std::cout << "Can't receive message from Server. Error # " << WSAGetLastError() << '\n';
-	//		closesocket(ClientSock);
-	//		WSACleanup();
-	//		return 1;
-	//	}
-	//	else
-	//		std::cout << "Server message: " << servBuff.data() << '\n';
-
-	//}
+	recv_file(&ClientSock);
+	system("pause");
 
 	closesocket(ClientSock);
 	WSACleanup();
-
-	system("pause");
 	return 0;
 }
--- ServerS/main.cpp
+++ ServerS/main.cpp
@@ -1,21 +1,18 @@
 #define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
 
 #include <iostream>
-#include <WinSock2.h>
-#include <WS2tcpip.h>
-#include <stdio.h>
-#include <string>
-#include <vector>
 #include <fstream>
+#include <string>
 #include <experimental/filesystem>
 
+#pragma(disable:4996)
 #pragma comment(lib, "Ws2_32.lib")
+#include <WinSock2.h>
+#include <WS2tcpip.h>
 
-//const char IP_SERV[] = "";			// Enter local Server IP address
-//const int PORT_NUM = 0;				// Enter Open working server port
-const short BUFF_SIZE = 1024;
+using namespace std;
 
-void SendFile(SOCKET* sock, const std::string& file_name) {
+void send_file(SOCKET* sock, const std::string& file_name) {
 	std::fstream file;
 	file.open(file_name, std::ios_base::in | std::ios_base::binary);
 	if (file.is_open()) {
@@ -30,9 +27,10 @@ void SendFile(SOCKET* sock, const std::string& file_name) {
 		send(*sock, std::to_string(file_size).c_str(), 16, 0);
 		send(*sock, file_name.c_str(), 32, 0);
 		send(*sock, bytes, file_size, 0);
+		delete[] bytes;
 	}
 	else std::cout << "Error file open\n";
-		file.close();
+	file.close();
 }
 
 int main() {
@@ -44,6 +42,13 @@ int main() {
 	}
 	else	std::cout << "WinSock initialization is OK" << '\n';
 
+	sockaddr_in addr_info;
+	ZeroMemory(&addr_info, sizeof(addr_info));
+	int size_addr = sizeof(addr_info);
+	InetPton(AF_INET, L"127.0.0.1", &addr_info.sin_addr);
+	addr_info.sin_port = htons(1111);
+	addr_info.sin_family = AF_INET;
+
 	SOCKET ServSock = socket(AF_INET, SOCK_STREAM, 0);
 	if (ServSock == INVALID_SOCKET) {
 		std::cout << "Error initialization socket # " << WSAGetLastError() << '\n';
@@ -53,20 +58,7 @@ int main() {
 	}
 	else	std::cout << "Server socket initialization is OK" << '\n';
 
-	in_addr ip_to_num;
-	if (inet_pton(AF_INET, "127.0.0.1", &ip_to_num) <= 0) {
-		std::cout << "Error in IP translation to special numeric format" << '\n';
-		return 1;
-	}
-
-	sockaddr_in servInfo;
-	ZeroMemory(&servInfo, sizeof(servInfo));
-	servInfo.sin_family = AF_INET;
-	//servInfo.sin_addr = ip_to_num;
-	servInfo.sin_addr.S_un.S_addr = INADDR_ANY;
-	servInfo.sin_port = htons(1111);
-
-	if (bind(ServSock, (sockaddr*)&servInfo, sizeof(servInfo)) != 0) {
+	if (bind(ServSock, (sockaddr*)&addr_info, sizeof(addr_info)) != 0) {
 		std::cout << "Error Socket binding to server info. Error # " << WSAGetLastError() << '\n';
 		closesocket(ServSock);
 		WSACleanup();
@@ -82,10 +74,7 @@ int main() {
 	}
 	else	std::cout << "Listening..." << '\n';
 
-	sockaddr_in clientInfo;
-	ZeroMemory(&clientInfo, sizeof(clientInfo));
-	int clientInfo_size = sizeof(clientInfo);
-	SOCKET ClientConn = accept(ServSock, (sockaddr*)&clientInfo, &clientInfo_size);
+	SOCKET ClientConn = accept(ServSock, (sockaddr*)&addr_info, &size_addr);
 	if (ClientConn == INVALID_SOCKET) {
 		std::cout << "Client detected, but can't connect to a client. Error # " << WSAGetLastError() << '\n';
 		closesocket(ServSock);
@@ -94,56 +83,15 @@ int main() {
 		return 1;
 	}
 	else {
-		std::cout << "Connection to a client established successfully" << '\n';
-		std::string path;
-		std::cin >> path;
-		SendFile(&ClientConn, path);
+		std::cout << "Connection established SUCCESSFULLY. Ready to send a picture to Client" << '\n';
 	}
-
-	//std::vector <char> servBuff(BUFF_SIZE), clientBuff(BUFF_SIZE);							// Creation of buffers for sending and receiving data
-	//short packet_size = 0;												// The size of sending / receiving packet in bytes
-
-	//while (true) {
-	//	packet_size = recv(ClientConn, servBuff.data(), servBuff.size(), 0);					// Receiving packet from client. Program is waiting (system pause) until receive
-	//	std::cout << "Client's message: " << servBuff.data() << '\n';
-
-	//	std::cout << "Your (host) message: ";
-	//	fgets(clientBuff.data(), clientBuff.size(), stdin);
-
-	//	// Check whether server would like to stop chatting 
-	//	if (clientBuff[0] == 'x' && clientBuff[1] == 'x' && clientBuff[2] == 'x') {
-	//		shutdown(ClientConn, SD_BOTH);
-	//		closesocket(ServSock);
-	//		closesocket(ClientConn);
-	//		WSACleanup();
-	//		return 0;
-	//	}
-	//	else if (clientBuff[0] == 'p' && clientBuff[1] == 'n' && clientBuff[2] == 'j') {
-
-	//		shutdown(ClientConn, SD_BOTH);
-	//		closesocket(ServSock);
-	//		closesocket(ClientConn);
-	//		WSACleanup();
-	//		return 0;
-	//	}
-
-	//	packet_size = send(ClientConn, clientBuff.data(), clientBuff.size(), 0);
-
-	//	if (packet_size == SOCKET_ERROR) {
-	//		std::cout << "Can't send message to Client. Error # " << WSAGetLastError() << '\n';
-	//		closesocket(ServSock);
-	//		closesocket(ClientConn);
-	//		WSACleanup();
-	//		return 1;
-	//	}
-
-	//}
+	std::string path;
+	std::cin >> path;
+	send_file(&ClientConn, path);
+	system("pause");
 
 	closesocket(ServSock);
 	closesocket(ClientConn);
 	WSACleanup();
-
-
-	system("pause");
 	return 0;
 }
